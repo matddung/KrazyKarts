97f0ebac01e5c3f6013edc62078cc8dbc3fdac29
matddung <matddung1111@gmail.com>
2025-04-16 18:40:48 +0900

    first commit

---
+++ Source/KrazyKarts/GoKart.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoKart.h"
+
+// Sets default values
+AGoKart::AGoKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector Force;
+	FVector Acceleration = Force / Mass;
+
+	FVector Translation = Velocity * DeltaTime * 100;
+
+	AddActorWorldOffset(Translation);
+}
+
+// Called to bind functionality to input
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForword);
+}
+
+void AGoKart::MoveForword(float Value)
+{
+	Velocity = GetActorForwardVector() * 20 * Value;
+}
